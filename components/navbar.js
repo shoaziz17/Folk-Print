eaa82c253b32c1d538fd940596c2663d0a0dc432
Abdusamatovv <abdusamatovmadiar@gmail.com>
2022-09-09 13:45:47 +0500

    added responsive navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,16 @@
 import styles from "./Navbar.module.scss";
 import { FaTelegram, FaFacebook, FaInstagram } from "react-icons/fa";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 function NavigationBar() {
+  const [open, setOpen] = useState(false);
+  function openMenu() {
+    open ? setOpen(false) : setOpen(true);
+  }
   return (
     <>
       {/* // Desktop navbar */}
-      <nav className={styles.navbar} id='logo'>
+      <nav className={styles.navbar} id="logo">
         <div className={styles.container}>
           <h1>Folkprint</h1>
           <ul>
@@ -15,9 +20,15 @@ function NavigationBar() {
             <li>Bizning ishlar</li>
           </ul>
           <div className={styles.socialIcon}>
-            <div><FaTelegram /></div>
-            <div><FaFacebook /></div>
-            <div><FaInstagram /></div>
+            <div>
+              <FaTelegram />
+            </div>
+            <div>
+              <FaFacebook />
+            </div>
+            <div>
+              <FaInstagram />
+            </div>
           </div>
         </div>
       </nav>
@@ -26,19 +37,42 @@ function NavigationBar() {
       <nav className={styles.mobileNavbar}>
         <div className={styles.container}>
           <h1>Folkprint</h1>
-          <AiOutlineMenu className="h-8 w-8 cursor-pointer duration-200 active:scale-95" />
+          <AiOutlineMenu
+            onClick={openMenu}
+            style={{ display: open ? "none" : "block" }}
+            className="h-8 w-8 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+          />
+          <AiOutlineClose
+            onClick={openMenu}
+            style={{ display: open ? "block" : "none" }}
+            className="h-8 w-8 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+          />
         </div>
-        <div>
-          <ul>
-            <li>Poligrafiya</li>
-            <li>Tashqi reklama</li>
-            <li>Akssesuarlar</li>
-            <li>Bizning ishlar</li>
-          </ul>
-          <div className={styles.socialIcon}>
-            <div><FaTelegram /></div>
-            <div><FaFacebook /></div>
-            <div><FaInstagram /></div>
+        <div
+          style={{ display: open ? "block" : "none" }}
+          className="animate__animated animate__fadeInDown h-full"
+        >
+          <div
+            style={{ display: open ? "block" : "none" }}
+            className="py-5 bg-zinc-900 hidden"
+          >
+            <ul>
+              <li>Poligrafiya</li>
+              <li>Tashqi reklama</li>
+              <li>Akssesuarlar</li>
+              <li>Bizning ishlar</li>
+            </ul>
+            <div className={styles.socialIcon}>
+              <div>
+                <FaTelegram />
+              </div>
+              <div>
+                <FaFacebook />
+              </div>
+              <div>
+                <FaInstagram />
+              </div>
+            </div>
           </div>
         </div>
       </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import 'animate.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
