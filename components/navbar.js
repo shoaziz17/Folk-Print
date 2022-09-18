bda5bee32158dcefb9da8e41eb35ef819a6c5e85
Abdusamatovv <abdusamatovmadiar@gmail.com>
2022-09-18 19:22:26 +0500

    added responsive tab navabar

--- components/navbar.js
+++ components/navbar.js
@@ -40,12 +40,12 @@ function NavigationBar() {
           <AiOutlineMenu
             onClick={openMenu}
             style={{ display: open ? "none" : "block" }}
-            className="h-8 w-8 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+            className="h-10 w-10 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
           />
           <AiOutlineClose
             onClick={openMenu}
             style={{ display: open ? "block" : "none" }}
-            className="h-8 w-8 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+            className="h-10 w-10 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
           />
         </div>
         <div
