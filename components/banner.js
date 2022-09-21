600e3a607b3746be717c1ac8de80939ef0cd0489
Abdusamatovv <abdusamatovmadiar@gmail.com>
2022-09-21 16:42:07 +0500

    added carousel

--- components/banner.js
+++ components/banner.js
@@ -1,17 +1,89 @@
-import styles from "../styles/Banner.module.scss"
+import styles from "../styles/Banner.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper";
 function Banner() {
-    return (
-        <div className={styles.banner}>
-            <img src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600" alt="banner image" />
-            <div className={styles.darker}>
-                <h1>Custom printed shirts with your own logo!</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, sapiente sed. Tenetur soluta cum dignissimos!</p>
-                <div className={styles.btnContent}>
-                    <button className={styles.btn}>Show catalogue</button>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.banner}>
+      <Swiper
+        navigation={false}
+        loop={true}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="banner image"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+            alt="banner image"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1529720317453-c8da503f2051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="banner image"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className={styles.darker}>
+      <Swiper
+        navigation={false}
+        loop={true}
+        // pagination={{
+        //   dynamicBullets: false,
+        // }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[ Navigation, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <h1>1 Custom printed shirts with your own logo!</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+          sapiente sed. Tenetur soluta cum dignissimos!
+        </p>
+        </SwiperSlide>
+        <SwiperSlide>
+        <h1>2 Custom printed shirts with your own logo!</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+          sapiente sed. Tenetur soluta cum dignissimos!
+        </p>
+        </SwiperSlide>
+        <SwiperSlide>
+        <h1>3 Custom printed shirts with your own logo!</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+          sapiente sed. Tenetur soluta cum dignissimos!
+        </p>
+        </SwiperSlide>
+      </Swiper>
+        
+        {/* <div className={styles.btnContent}>
+          <button className={styles.btn}>Show catalogue</button>
+        </div> */}
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
