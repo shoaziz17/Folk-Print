f7e031be9b043504b4f75e22f4f94ac454ed5a65
Abdusamatovv <abdusamatovmadiar@gmail.com>
2022-09-22 08:29:10 +0500

    deleted logo svg img

--- components/banner.js
+++ components/banner.js
@@ -30,7 +30,7 @@ function Banner() {
         </SwiperSlide>
         <SwiperSlide>
           <img
-            src="https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+            src="https://i.postimg.cc/prVJ7J9L/2836242.jpg"
             alt="banner image"
           />
         </SwiperSlide>
