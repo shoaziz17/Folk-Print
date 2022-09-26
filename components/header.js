aba5d826b4e013bf23ed2dc0eaa5c01a5ec48273
Abdusamatovv <abdusamatovmadiar@gmail.com>
2022-09-26 15:39:05 +0500

    added new header

--- components/header.js
+++ components/header.js
@@ -1,11 +1,42 @@
-import NavigationBar from "./navbar"
-
+import styles from "../styles/Header.module.scss";
+import { FaTelegram, FaFacebook, FaInstagram } from "react-icons/fa";
 function Header() {
-    return (
-        <div>
-            <NavigationBar />
+  return (
+    <header className={styles.header}>
+      <img
+        className={styles.bannerImg}
+        src="https://images.unsplash.com/photo-1529720317453-c8da503f2051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        alt="banner-img"
+      />
+      <div className={styles.darker}>
+        <nav className={styles.navbar}>
+          <img src="https://i.postimg.cc/QdzFKJzz/001.png" alt="logo" />
+            <div className={styles.socialIcon}>
+              <div>
+                <FaTelegram className="w-10 h-10 cursor-pointer active:scale-95 duration-200" />
+              </div>
+              <div>
+                <FaFacebook className="w-10 h-10 cursor-pointer active:scale-95 duration-200" />
+              </div>
+              <div>
+                <FaInstagram className="w-10 h-10 cursor-pointer active:scale-95 duration-200" />
+              </div>
+              <div>
+                <a className="bg-white rounded-[50px] py-3 px-5 text-black font-bold " href="tel:+998 33 338 86 08">+998 33 338 86 08</a>
+              </div>
+            </div>
+        </nav>
+        <div className={styles.textSection}>
+            <h1>Фабрика принтов №1 в Узбекистане.</h1>
+            <h2>Все виды изготовление принтов с вашим логотипом</h2>
         </div>
-    )
+        <div className={styles.buttons}>
+          <h1 className="bg-yellow-400 py-3 px-5 rounded-[50px]  text-lg font-semibold hover:cursor-pointer hover:bg-yellow-300 active:scale-95 duration-200">Наш каталог</h1>
+          <h1 className="text-white text-lg border-2 py-3 px-5 rounded-[50px] font-semibold hover:cursor-pointer hover:bg-white hover:text-black active:scale-95 duration-200">Наши контакты</h1>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- components/navbar.js
+++ components/navbar.js
@@ -10,75 +10,79 @@ function NavigationBar() {
     open ? setOpen(false) : setOpen(true);
   }
   return (
-    <>
-      {/*desktop navbar*/}
-      <nav className={styles.navbar} id="logo">
-        <div className={styles.container}>
-        <img className={styles.logoImg} src="https://i.postimg.cc/rsHQKPnd/001.png" alt="logo"/>
-          <ul>
-            <li> <Link href={"#about"}>О нас</Link></li>
-            <li> <Link href={"#portfolio"}>Портфолио</Link></li>
-            <li> <Link href={"#partners"}>Партнеры</Link></li>
-            <li> <Link href={"#location"}>Локация</Link></li>
-          </ul>
-          <div className={styles.socialIcon}>
-            <div>
-              <FaTelegram />
-            </div>
-            <div>
-              <FaFacebook />
-            </div>
-            <div>
-              <FaInstagram />
-            </div>
-          </div>
-        </div>
-      </nav>
-      {/* // Mobile navbar  */}
-      <nav className={styles.mobileNavbar}>
-        <div className={styles.container}>
-        <img className={styles.logoImg} src="https://i.postimg.cc/rsHQKPnd/001.png" alt="logo"/>
-          <AiOutlineMenu
-            onClick={openMenu}
-            style={{ display: open ? "none" : "block" }}
-            className="h-10 w-10 | cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
-          />
-          <AiOutlineClose
-            onClick={openMenu}
-            style={{ display: open ? "block" : "none" }}
-            className="h-10 w-10 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
-          />
-        </div>
-        <div
-          style={{ display: open ? "block" : "none" }}
-          className="animate__animated animate__fadeInDown h-full absolute z-20 w-full"
-        >
-          <div
-            style={{ display: open ? "block" : "none" }}
-            className="py-5 bg-zinc-900 hidden h-screen"
-          >
-            <ul>
-              <li>Poligrafiya</li>
-              <li>Tashqi reklama</li>
-              <li>Akssesuarlar</li>
-              <li>Bizning ishlar</li>
-            </ul>
-            <div className={styles.socialIcon}>
-              <div>
-                <FaTelegram />
-              </div>
-              <div>
-                <FaFacebook />
-              </div>
-              <div>
-                <FaInstagram />
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-      {/* // Mobile navbar  */}
-    </>
+      <div>
+        
+      </div>
+    // <>
+    //   {/*desktop navbar*/}
+    //   <nav className={styles.navbar} id="logo">
+    //     <div className={styles.container}>
+    //     <img className={styles.logoImg} src="https://i.postimg.cc/rsHQKPnd/001.png" alt="logo"/>
+    //       <ul>
+    //         <li> <Link href={"#about"}>О нас</Link></li>
+    //         <li> <Link href={"#portfolio"}>Портфолио</Link></li>
+    //         <li> <Link href={"#partners"}>Партнеры</Link></li>
+    //         <li> <Link href={"#location"}>Локация</Link></li>
+    //       </ul>
+    //       <div className={styles.socialIcon}>
+    //         <div>
+    //           <FaTelegram />
+    //         </div>
+    //         <div>
+    //           <FaFacebook />
+    //         </div>
+    //         <div>
+    //           <FaInstagram />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </nav>
+    //   {/* // Mobile navbar  */}
+    //   <nav className={styles.mobileNavbar}>
+    //     <div className={styles.container}>
+    //     <img className={styles.logoImg} src="https://i.postimg.cc/rsHQKPnd/001.png" alt="logo"/>
+    //       <AiOutlineMenu
+    //         onClick={openMenu}
+    //         style={{ display: open ? "none" : "block" }}
+    //         className="h-10 w-10 | cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+    //       />
+    //       <AiOutlineClose
+    //         onClick={openMenu}
+    //         style={{ display: open ? "block" : "none" }}
+    //         className="h-10 w-10 cursor-pointer duration-200 active:scale-95 text-yellow-300 animate__animated animate__rotateIn"
+    //       />
+    //     </div>
+    //     <div
+    //       style={{ display: open ? "block" : "none" }}
+    //       className="animate__animated animate__fadeInDown h-full absolute z-20 w-full"
+    //     >
+    //       <div
+    //         style={{ display: open ? "block" : "none" }}
+    //         className="py-5 bg-zinc-900 hidden h-screen"
+    //       >
+    //         <ul>
+    //           <li>Poligrafiya</li>
+    //           <li>Tashqi reklama</li>
+    //           <li>Akssesuarlar</li>
+    //           <li>Bizning ishlar</li>
+    //         </ul>
+    //         <div className={styles.socialIcon}>
+    //           <div>
+    //             <FaTelegram />
+    //           </div>
+    //           <div>
+    //             <FaFacebook />
+    //           </div>
+    //           <div>
+    //             <FaInstagram />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </nav>
+    //   {/* // Mobile navbar  */}
+    // </>
+    
   );
 }
 
